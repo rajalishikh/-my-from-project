2d80500e38d38bde2d837822bd2de0d29bb5b17d
raj ali sheikhe <nahidujjamanraj@gmail.comm>
2024-03-31 12:29:12 +0600

    Now I am add the cusrole bye using useEffect and call focus funtion

--- src/Components/NameRefer.jsx
+++ src/Components/NameRefer.jsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 const NameRefer = () => {
 
@@ -8,15 +8,23 @@ const NameRefer = () => {
         console.log(useEmail.current.value)
 
     }
+    // use hooks for cursol 
+    
     // hooks of referce
     const useName = useRef(null);
     const useEmail = useRef(null);
+
+    useEffect(() => {
+        useName.current.focus();
+
+    },[])
     return (
         <div>
              <form className="mb-3 text-center" onSubmit={handleSubmit}>
                 <input ref={useName} className="mb-4 mt-3" type="text" name="name" /><br />
-               <input type="email" name="email" id="" ref={useEmail}/><br />
+                <input type="email" name="email" id="" ref={useEmail} /><br />
                 <button className="bg-red-500 p-3 m-3 rounded">Submit</button>
+
             </form>
             
         </div>
