cbca29b398430da69a78dfcefcc338c6c2662c9e
raj ali sheikhe <nahidujjamanraj@gmail.comm>
2024-04-04 11:04:33 +0600

    In this section I add usecontext api and add some money

--- src/Components/Brother/Brother.jsx
+++ src/Components/Brother/Brother.jsx
@@ -1,9 +1,15 @@
+import { useContext } from "react";
+import { FoodContext } from "../Grandpa/Grandpa";
 
 
 const Brother = () => {
+    const [money, setMoney] = useContext(FoodContext);
     return (
         <div>
             <h2 className="border-solid border-2 border-yellow-500 m-6 p-6">Brother</h2>
+            <p>Today I bring some Food for my brother which price is {money}</p>
+            <button className="bg-teal-800 p-3 m-2" onClick={()=>setMoney(money+1000)}> Add more money</button>
+
             
         </div>
     );
--- src/Components/Grandpa/Grandpa.jsx
+++ src/Components/Grandpa/Grandpa.jsx
@@ -1,22 +1,28 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import Son from "../Son/Son";
 import Uncle from "../Uncle/Uncle";
 import Unty from "../Unty/Unty";
 
 export const AssetContext = createContext('Dimond');
+export const FoodContext = createContext();
+
 
 
 const Grandpa = () => {
+    const[money,setMoney]=useState(1000)
     return (
         <div className=" border-solid border-2 border-yellow-500 m-6 p-6">
             <h1 className="text-2xl text-center">Grandpa</h1>
-            <AssetContext.Provider value={'Gold'}>
+            <p>My grandpa Have total money { money}</p>
+            <FoodContext.Provider value={[money,setMoney]}>
+            <AssetContext.Provider value={'Gold Ring'}>
             <section className="flex justify-around ">
                 <Son></Son>
                 <Uncle></Uncle>
                 <Unty></Unty>
             </section>
             </AssetContext.Provider>
+            </FoodContext.Provider>
             
         </div>
     );
